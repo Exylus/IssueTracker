57292e3a8fdec0cb2ea3ff896658fe00a4ce4102
Erwan <thomy.erwan@gmail.com>
2020-02-04 11:50:05 +0100

    added navbar

--- index.php
+++ index.php
@@ -5,6 +5,28 @@
     <title>OwlSight - Flexible Issue Tracker</title>
   </head>
   <body>
-
+    <!-- NAVBAR -->
+    <nav class="top-nav">
+			<ul class="nav-list">
+				<li>
+					<a href="index.php" class="nav-link">Home</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link">Overview</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link">To Do</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link">In Progress</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link">Code Review</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link">Done</a>
+				</li>
+			</ul>
+		</nav>
   </body>
 </html>
