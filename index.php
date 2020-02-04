9ff5dc84b4f01bb05ca9166471c1290b45c918e3
Erwan <thomy.erwan@gmail.com>
2020-02-04 15:08:38 +0100

    added section1 & 2

--- index.php
+++ index.php
@@ -24,10 +24,16 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     </div>
     <div class="topbg">
       <div id="content">
-        <section id="Section1">
+        <section id="section1">
           <div class="OwlsightPres">
-            <h2>OwlSight</h2>
+            <h2 data-aos="slide-left">OwlSight</h2>
             <p>Clean. Efficient.<br>Track your bugs.</p>
+          </div>
+        </section>
+        <section id="section2" data-aos="flip-left" data-aos-delay="500">
+          <div class="Performant">
+            <h2>Simple as can be</h2>
+            <p>New decade, New look;$, New <b>YOU</b> <br> Why not try <b>OwlSight Issue Tracker</b> ?</p>
           </div>
         </section>
       </div>
